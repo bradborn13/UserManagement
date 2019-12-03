3a6fa66bd31cd36166f2016aeb00af2f6f42cdd2
Constantin <clc.aarhusit@gmail.com>
2019-12-03 19:07:36 +0100

    components folowing the new .eslint rules

--- src/components/OwnerComponents/OwnerAccounts/OwnerAccounts.js
+++ src/components/OwnerComponents/OwnerAccounts/OwnerAccounts.js
@@ -1,39 +1,36 @@
 import React from 'react';
-import { Col,Row,Table } from 'react-bootstrap';
+import { Col, Row, Table } from 'react-bootstrap';
 import Moment from 'react-moment';
 
-const ownerAccounts = (props) =>{
-    let accounts = null;
-    if(props.accounts){
-        accounts = props.accounts.map(account =>{
-            return(
-                <tr key={account.id}>
-                    <td>{account.accountType}</td>
-                    <td><Moment format="DD/MM/YYYY">{account.dateCreated}</Moment></td>
-                </tr>
-            );
-        })
-    }
-return(
+const ownerAccounts = props => {
+  let accounts = null;
+  if (props.accounts) {
+    accounts = props.accounts.map(account => {
+      return (
+        <tr key={account.id}>
+          <td>{account.accountType}</td>
+          <td>
+            <Moment format="DD/M    M/YYYY">{account.dateCreated}</Moment>
+          </td>
+        </tr>
+      );
+    });
+  }
+  return (
     <Row>
-        <Col md={12}>
-            <Table responsive striped>
-                <thead>
-                    <tr>
-                        <th>Account Type</th>
-                        <th>Date Created</th>
-                    </tr>
-                </thead>
-                <tbody>
-
-                    {accounts}
-                </tbody>
-            </Table>
-        </Col>
+      <Col md={12}>
+        <Table responsive striped>
+          <thead>
+            <tr>
+              <th>Account Type</th>
+              <th>Date Created</th>
+            </tr>
+          </thead>
+          <tbody>{accounts}</tbody>
+        </Table>
+      </Col>
     </Row>
-)
+  );
+};
 
-
-}
-
-export default ownerAccounts
+export default ownerAccounts;
